a8199d3ab42830d8576448dc67a202006f8e4642
kazuya kawaguchi <kawakazu80@gmail.com>
2012-03-29 22:47:11 +0900

    update pythonbrew setting.

--- .bashrc
+++ .bashrc
@@ -14,11 +14,6 @@ init_dir_colors
 
 init_env
 
-#  python virtal enviroment
-#source /opt/local/Library/Frameworks/Python.framework/Versions/2.5/bin/virtualenvwrapper.sh
-#source /opt/local/Library/Frameworks/Python.framework/Versions/2.6/bin/virtualenvwrapper.sh
-
-
 # add configration from '__config__'
 init_add_config
 
--- __config__/pythonbrew.sh
+++ __config__/pythonbrew.sh
@@ -1,4 +1,10 @@
 if [ -s "$HOME/.pythonbrew/etc/bashrc" ]; then
     source "$HOME/.pythonbrew/etc/bashrc"
+    # TODO: extract python version "2.x.y"
     pythonbrew use 2.7.2 1>/dev/null 2>&1
+    if [ -f "$HOME/.pythonbrew/pythons/Python-2.7.2/bin/python" ]; then
+        export VIRTUALENVWRAPPER_PYTHON=$HOME/.pythonbrew/pythons/Python-2.7.2/bin/python
+        export WORKON_HOME=$HOME/.virtualenvs
+        source $HOME/.pythonbrew/pythons/Python-2.7.2/bin/virtualenvwrapper.sh
+    fi
 fi
