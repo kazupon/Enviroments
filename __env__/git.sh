dc82c8790c6d7ce21d25c7ef4264b6b307540c8c
kazuya kawaguchi <kawakazu80@gmail.com>
2014-01-19 03:50:03 +0900

    Modify color code

--- __env__/git.sh
+++ __env__/git.sh
@@ -2,5 +2,5 @@ GIT_UTIL_PATH=$HOME/homebrew/etc/bash_completion.d
 source "${GIT_UTIL_PATH}/git-prompt.sh"
 source "${GIT_UTIL_PATH}/git-completion.bash"
 GIT_PS1_SHOWDIRTYSTATE=true
-export PS1='\[\033[32m\]\u@\h\[\033[00m\]:\[\033[34m\]\w\[\033[31m\]$(__git_ps1)\[\033[00m\]\$ '
+export PS1='\[\033[1;32m\]\u@\h\[\033[1;00m\]:\[\033[1;34m\]\w\[\033[1;31m\]$(__git_ps1)\[\033[1;00m\]\$ '
 source "${GIT_UTIL_PATH}/tig-completion.bash"
