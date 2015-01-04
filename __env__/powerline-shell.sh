a9a1a7f2f3dce3518110a31d941992a302962e72
kazuya kawaguchi <kawakazu80@gmail.com>
2015-01-05 02:26:35 +0900

    Update powerline-shell.sh

--- __env__/powerline-shell.sh
+++ __env__/powerline-shell.sh
@@ -2,4 +2,8 @@ function _update_ps1() {
    export PS1="$(~/.enviroments/powerline/shell/powerline-shell.py $? 2> /dev/null)"
 }
 
-export PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+if [[ -d "$HOME/.enviroments/powerline" ]]; then
+    if [[ -f "$HOME/.enviroments/powerline/shell/powerline-shell.py" ]]; then
+        export PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"
+    fi
+fi
