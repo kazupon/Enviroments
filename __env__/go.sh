fd76a2aeb8cb7ed3e63dc7476f121e77ababc79c
kazuya kawaguchi <kawakazu80@gmail.com>
2015-01-06 02:20:39 +0900

    Add bashrc files

---
+++ __env__/go.sh
@@ -0,0 +1,3 @@
+export GOPATH=$HOME/.go
+export GOROOT=$HOME/homebrew/opt/go/libexec
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
---
+++ __env__/vagrant_digital_ocean.sh
@@ -0,0 +1,1 @@
+export SSL_CERT_FILE=$HOME/homebrew/opt/curl-ca-bundle/share/ca-bundle.crt
