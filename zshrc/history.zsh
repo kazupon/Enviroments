0fb060a170bd5d4669bc1dc22a12ec9b3c83e429
kazuya kawaguchi <kawakazu80@gmail.com>
2015-01-07 19:59:52 +0900

    Update history zshrc

--- zshrc/history.zsh
+++ zshrc/history.zsh
@@ -1,10 +1,13 @@
-setopt hist_ignore_all_dups
-#setopt hist_ignore_dups
-setopt hist_reduce_blanks
-#setopt hist_ignore_spece
-setopt hist_no_store
+HISTFILE=~/.zsh_history
+HISTSIZE=10000000
+SAVEHIST=10000000
+setopt share_history
+#setopt append_history
 #setopt inc_append_history
-#setopt share_history
-histfile=~/.zsh_history
-histsize=10000000
-savehist=10000000
+setopt hist_no_store
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_ignore_all_dups
+setopt hist_reduce_blanks
+setopt hist_ignore_space
+#setopt extended_history
